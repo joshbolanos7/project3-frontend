3be11432e0b1f300563fc1c238ec959de93b6cde
Joshua Bolanos <josh.bolanos7@gmail.com>
2021-10-21 16:55:43 -0400

    able to see drivers and create.

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,13 +1,37 @@
-import { Route, Switch } from " react-router-dom";
+import { useEffect, useState } from "react";
+import { Route, Switch } from "react-router-dom";
 import Index from "../pages/Index";
 import Show from "../pages/Show";
 
 function Main(props) {
+    const [driver, setDriver] = useState(null);
+
+    const URL = "https://josh-project3-backend.herokuapp.com/driver/";
+
+    const getDriver = async () => {
+        const response = await fetch(URL);
+        const data = await response.json();
+        setDriver(data);
+    };
+
+    const createDriver = async (driver) => {
+        await fetch(URL, {
+            method: "POST",
+            headers: {
+                "Content-Type": "Application/json",
+            },
+            body: JSON.stringify(driver),
+        });
+        getDriver();
+    };
+
+    useEffect(() => getDriver(), []);
+
     return (
-        <Main>
-            <Swtich>
+        <main>
+            <Switch>
                 <Route exact path="/">
-                    <Index />
+                    <Index driver={driver} createDriver={createDriver} />
                 </Route>
                 <Route
                     path="/driver/:id"
@@ -17,9 +41,9 @@ function Main(props) {
                         />
                     )}
                 />
-            </Swtich>
-        </Main>
-    )
+            </Switch>
+        </main>
+    );
 }
 
 export default Main;
---
+++ src/pages/Index.js
@@ -0,0 +1,79 @@
+import { useState } from "react";
+import { Link } from "react-router-dom"
+
+function Index(props) {
+
+    const [newForm, setNewForm] = useState({
+        name: "",
+        image: "",
+        team: "",
+        rating: "",
+    });
+
+    const handleChange = (event) => {
+        setNewForm({ ...newForm, [event.target.name]: event.target.value });
+    };
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        props.createDriver(newForm);
+        setNewForm({
+            name: "",
+            image: "",
+            team: "",
+            rating: "",
+        });
+    };
+    const loaded = () => {
+        return props.driver.map((driver) => (
+            <div key={driver._id} className="driver">
+                <Link to={`/driver/${driver._id}`}><h1>{driver.name}</h1></Link>
+                <img src={driver.image} alt={driver.name} />
+                <h3>{driver.team}</h3>
+            </div>
+        ));
+    };
+
+    const loading = () => {
+        return <h1>Loading...</h1>;
+    };
+
+    return (
+        <section>
+            <form onSubmit={handleSubmit}>
+                <input
+                    type="text"
+                    value={newForm.name}
+                    name="name"
+                    placeholder="name"
+                    onChange={handleChange}
+                />
+                <input
+                    type="text"
+                    value={newForm.image}
+                    name="image"
+                    placeholder="image URL"
+                    onChange={handleChange}
+                />
+                <input
+                    type="text"
+                    value={newForm.team}
+                    name="team"
+                    placeholder="team"
+                    onChange={handleChange}
+                />
+                <input
+                    type="text"
+                    value={newForm.rating}
+                    name="rating"
+                    placeholder="rating"
+                    onChange={handleChange}
+                />
+                <input type="submit" value="Create Driver" />
+            </form>
+            {props.driver ? loaded() : loading()}
+        </section>
+    );
+}
+
+export default Index;
